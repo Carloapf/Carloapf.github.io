900cd01a37add6ed42369454251cda94be9387b6
Carloapf <carlo.pano@uabc.edu.mx>
2022-09-04 21:18:55 -0700

    Using a Custom Home Page

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -5,12 +5,13 @@ import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@mui/icons-material/Book';
 import UserIcon from '@mui/icons-material/Group';
+import Dashboard from './Dashboard';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 //const App = () => <Admin dataProvider={dataProvider} />;
 const App = () => (
-      <Admin dataProvider={dataProvider}>
+      <Admin dashboard={Dashboard} dataProvider={dataProvider}>
           <Resource name="users" list={UserList} icon={UserIcon} recordRepresentation="name" />
           <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
       </Admin>
