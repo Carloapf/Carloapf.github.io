ccf0d22231dda8584cbad97f1dde6726423e7c76
Carloapf <carlo.pano@uabc.edu.mx>
2022-09-04 21:34:11 -0700

    Supporting Mobile Devices

--- test-admin/src/posts.js
+++ test-admin/src/posts.js
@@ -1,21 +1,35 @@
 import * as React from "react";
-import { List, Datagrid, TextField, ReferenceField, Edit, EditButton,Create,SimpleForm,ReferenceInput,TextInput, useRecordContext} from 'react-admin';
+import { useMediaQuery} from '@mui/material';
+import { SimpleList, List, Datagrid, TextField, ReferenceField, Edit, EditButton,Create,SimpleForm,ReferenceInput,TextInput, useRecordContext} from 'react-admin';
 
 const postFilters = [
     <TextInput source="q" label="Search" alwaysOn />,
     <ReferenceInput source="userId" label="User" reference="users" />,
 ];
 
-export const PostList = porps => (
-    <List filters={postFilters}>
-       <Datagrid>
-           <TextField source="id" />
-            <ReferenceField source="userId" reference="users" />
-            <TextField source="title" />
-           <EditButton />
-        </Datagrid>
-    </List>
-); 
+export const PostList = porps => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.title}
+                    secondaryText={record => (
+                        <ReferenceField label="User" source="userId" reference="users" />
+                    )}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <ReferenceField label="User" source="userId" reference="users" />
+                    <TextField source="title" />
+                    <TextField source="body" />
+                    <EditButton />
+                </Datagrid>
+            )}
+        </List>
+    );
+}
 const PostTitle = () => {
     const record = useRecordContext();
     return <span>Post {record ? `"${record.title}"` : ''}</span>;
